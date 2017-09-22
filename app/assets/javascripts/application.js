c67dc692c8c25610d96f61a15cd536914b56d25f
Dimitris <admin@wiseinno.tech>
2017-09-22 20:21:02 +0300

    final push for today

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require rails-ujs
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$( function() {
+    $( "#tabs" ).tabs();
+  } );

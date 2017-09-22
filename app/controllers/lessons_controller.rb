6003b16c04080d3573f466f08425d0301a390263
Dimitris <admin@wiseinno.tech>
2017-09-22 19:14:18 +0300

    basic update is working

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,3 +1,15 @@
 class LessonsController < ApplicationController
   def index; end
+
+  def new
+    @lessons = Lesson.all
+  end
+
+  def submit_grades
+    Lesson.all.each do |les|
+      @grade = params[les.name]
+      les.update_attribute(:grade, @grade)
+    end
+    redirect_to action: 'index'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/lessons' => 'lessons#index'
+  post '/lessons/submit_grades' => 'lessons#submit_grades'
+  get '/lessons/new' => 'lessons#new'
 end

53d29f0676c578d3bfa931903840e9be26538acf
Dimitris <admin@wiseinno.tech>
2017-09-22 20:00:07 +0300

    some little design

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,4 @@
 class Lesson < ApplicationRecord
-  validates_uniqueness_of :code
-  validates :grade, inclusion: [in: [0..10]]
   before_save :default_values
   def default_values
     self.grade ||= 0

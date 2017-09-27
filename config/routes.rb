b087440a7db99c7eaa3f6232d32503331da41209
Dimitris <admin@wiseinno.tech>
2017-09-27 13:37:25 +0300

    installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post '/lessons/select' => 'lessons#select'
   get 'lessons/delete' => 'lessons#delete'
   post '/lessons/remove' => 'lessons#remove'
+  root '/'
 end

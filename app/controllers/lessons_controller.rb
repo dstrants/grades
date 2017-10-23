ae353cbfd89ea03abe490fa76763acae50927fa7
Dimitris <admin@wiseinno.tech>
2017-10-23 20:43:41 +0300

    switched new to grades

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,31 +1,33 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  def index; end
+  def index
+    @list = current_user.lessons.where(selected: true).group_by(&:semester)
+  end
 
-  def new
+  def grades
     @lessons = current_user.lessons.all
   end
 
   def semesters
-    @semesters = Lesson.where(selected: true).group_by(&:semester)
+    @semesters = current_user.lessons.where(selected: true).group_by(&:semester)
   end
 
   def show_semester
-    @semesters = Lesson.where(selected: true).group_by(&:semester)
+    @semesters = current_user.lessons.where(selected: true).group_by(&:semester)
     @semester = @semesters[params[:id].to_i]
   end
 
   def stats; end
 
   def add
-    @basics = Lesson.where(ltype: 2)
-    @generals = Lesson.where(ltype: 3)
-    @specials = Lesson.where(ltype: 4)
-    @thesis = Lesson.where(ltype: 5)
+    @basics = current_user.lessons.where(ltype: 2)
+    @generals = current_user.lessons.where(ltype: 3)
+    @specials = current_user.lessons.where(ltype: 4)
+    @thesis = current_user.lessons.where(ltype: 5)
   end
 
   def submit_grades
-    Lesson.all.each do |les|
+    current_user.lessons.all.each do |les|
       @grade = params[les.name]
       les.update_attribute(:grade, @grade)
     end
@@ -33,20 +35,20 @@ class LessonsController < ApplicationController
   end
 
   def select
-    @les = Lesson.find(params[:lesson])
+    @les = current_user.lessons.find(params[:lesson])
     @les.update_attribute(:selected, true) unless @les.selected
     redirect_to action: 'add'
   end
 
   def delete
-    @basics = Lesson.where(ltype: 2, selected: true)
-    @generals = Lesson.where(ltype: 3, selected: true)
-    @specials = Lesson.where(ltype: 4, selected: true)
-    @thesis = Lesson.where(ltype: 5, selected: true)
+    @basics = current_user.lessons.where(ltype: 2, selected: true)
+    @generals = current_user.lessons.where(ltype: 3, selected: true)
+    @specials = current_user.lessons.where(ltype: 4, selected: true)
+    @thesis = current_user.lessons.where(ltype: 5, selected: true)
   end
 
   def remove
-    @les = Lesson.find(params[:lesson])
+    @les = current_user.lessons.find(params[:lesson])
     @les.update_attribute(:selected, false) if @les.selected
     redirect_to action: 'new'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/lessons' => 'lessons#index'
   post '/lessons/submit_grades' => 'lessons#submit_grades'
-  get '/lessons/new' => 'lessons#new'
+  get '/lessons/grades' => 'lessons#grades'
   get '/lessons/add' => 'lessons#add'
   post '/lessons/select' => 'lessons#select'
   get 'lessons/delete' => 'lessons#delete'
